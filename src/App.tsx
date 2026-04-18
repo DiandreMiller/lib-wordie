f123dd1a0534368723289a9ef8854646fa0cf5d2
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2026-04-18 03:35:05 -0400

    to my heart page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from './Pages/HomePage';
 import GamePage from './Pages/GamePage';
+import ToMyHeart from "./Pages/ToMyHeart";
 import FOUROFOUR from "./Pages/FOUROFOUR";
 // import GamePageTest from "./Pages/GamePlayTest";
 import './App.css'
@@ -15,6 +16,28 @@ import './App.css'
 //add badges when you get 5, 10, 25, 50, 100 etc in a row
 // add friends
 
+// Catch-all near routes
+// function AliasRouter() {
+//   const { pathname } = useLocation();
+
+//   const redirectIf = (base: string, to: string) => {
+//       const re = new RegExp(`^/${base}[a-z0-9-]*$`, "i");
+//       return re.test(pathname) ? <Navigate to={`/${to}`} replace /> : null;
+//     };
+
+//   const hardRedirect = 
+//     redirectIf("cart", "cart") ||
+//     redirectIf("contactus", "contactus") ||
+//     redirectIf("emailsent", "emailsent") ||
+//     redirectIf("inventory", "inventory") ||
+//     redirectIf("meetjosh&jenna", "meetjosh&jenna");
+
+//   if(hardRedirect) {
+//     return hardRedirect;
+//   }
+// }
+
+
 function App() {
   return (
     <div>
@@ -22,6 +45,7 @@ function App() {
         <Routes>
           <Route element={<HomePage />} path='/' />
           <Route element={<GamePage />} path='/play' />
+          <Route element={<ToMyHeart />} path='/tomyheart' />
           {/* <Route element={<GamePageTest />} path='/test' /> */}
           <Route element={<FOUROFOUR />} path='*' />
         </Routes>
